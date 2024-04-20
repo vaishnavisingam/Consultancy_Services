081df764b6c73cbaf93f7b83ed1cc0e8496f909c
krishnaveni <singamvaishnavi99@gmail.com>
2024-04-20 15:07:29 +0530

    updated contact page

--- contact.php
+++ contact.php
@@ -3,13 +3,13 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $phone = $_POST['phone'];
-$address = $_POST['$address'];
+$address = $_POST['address']; // corrected variable name
 $message= $_POST['message'];
 
 $to = "singamvaishnavi99@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone =" . $phone . "\r\n address =" . $address;
-$headers = "From: noreply@3ghrservices" . "\r\n" .
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone =" . $phone . "\r\n Address =" . $address; // corrected variable name
+$headers = "From: noreply@3ghrservices.com" . "\r\n" . // corrected email address
 "CC: singamvaishnavi99@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);

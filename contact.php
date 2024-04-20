dab82f32c1025e933527eb49882acac546c2f620
krishnaveni <singamvaishnavi99@gmail.com>
2024-04-20 14:12:46 +0530

    Updated Contact PHP Code

--- contact.php
+++ contact.php
@@ -1,26 +1,31 @@
 <?php
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$address = $_POST['address'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $message = $_POST['message'];
 
+    // Email address of receiver
+    $to = "singamvaishnavi99@gmail.com";
 
-//email address of receiver
-$to = "singamvaishnavi99@gmail.com";
+    $subject = 'Mail from 3G HR Services in Hyderabad';
 
-$subject = 'Mail from 3G HR Services in Hyderabad';
+    // Compose email body
+    $txt = "Name: " . $name . "\r\n Email: " . $email . "\r\n Phone: " . $phone . "\r\n Address: " . $address . "\r\n Message: " . $message;
 
-$txt = "Name =" .$name. "\r\n Phone Number =" .$phone. "\r\n Email =" .$email. "\r\n Message =" .$message. "\r\n Address =" .$address;
+    // Additional headers
+    $headers = "From: " . $email . "\r\n" . "CC: ";
 
-$header = "From: " . "\r\n" ."CC: ";
-
-if($email!=NULL){
-    mail($to,$subject,$txt,$header);
+    // Send email
+    if (mail($to, $subject, $txt, $headers)) {
+        echo "Email sent successfully";
+    } else {
+        echo "Failed to send email";
+    }
 }
 
-//redirect
-{
-    header("location: index.html");
-}
+// Redirect back to the index page
+header("location: index.html");
 ?>

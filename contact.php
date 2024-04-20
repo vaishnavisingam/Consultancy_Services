cd50e5916b07c30cdc87ead34fa63db2363ac3a1
krishnaveni <singamvaishnavi99@gmail.com>
2024-04-20 14:48:40 +0530

    updated redirect page

--- contact.php
+++ contact.php
@@ -1,31 +1,19 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $address = $_POST['address'];
-    $message = $_POST['message'];
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$phone = $_POST['phone'];
+$address = $_POST['$address'];
+$message= $_POST['message'];
 
-    // Email address of receiver
-    $to = "singamvaishnavi99@gmail.com";
-
-    $subject = 'Mail from 3G HR Services in Hyderabad';
-
-    // Compose email body
-    $txt = "Name: " . $name . "\r\n Email: " . $email . "\r\n Phone: " . $phone . "\r\n Address: " . $address . "\r\n Message: " . $message;
-
-    // Additional headers
-    $headers = "From:noreply@3ghrservices.com " . $email . "\r\n" . "CC: ";
-
-    // Send email
-    if (mail($to, $subject, $txt, $headers)) {
-        echo "Email sent successfully";
-    } else {
-        echo "Failed to send email";
-    }
+$to = "singamvaishnavi99@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone =" . $phone . "\r\n address =" . $address;
+$headers = "From: noreply@vaishnavi99.com" . "\r\n" .
+"CC: singamvaishnavi99@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
-
-// Redirect back to the index page
-header("location: index.html");
+//redirect
+header("Location:index.html");
 ?>

70b641c026c6d9307c1a23bb6f8a230f32706eee
Krishnaveni <singamvaishnavi99@gmail.com>
2024-05-04 10:36:28 +0530

    Updated Code In Website

--- contact.php
+++ contact.php
@@ -11,16 +11,18 @@ $to = "singamvaishnavi99@gmail.com";
 
 $subject = 'Mail from 3ghr services in Hyderabad';
 
-$txt = "Name =" .$name. "\r\n Phone Number =" .$phoneNumber. "\r\n Email =" .$email. "\r\n Message =" .$message. "\r\n Address =" .$address;
+$txt = "name =" .$name. "\r\n email =" .$email. "\r\n phoneNumber=" .$phoneNumber. "\r\n address=" .$address. "\r\n message =" .$message;
 
 $header = "From: noreply@singamvaishnavi.com" . "\r\n" ."CC: singamvaishnavi99@gmail.com";
 
-if($email!=NULL){
-    mail($to,$subject,$txt,$header);
-}
-
-//redirect
-{
-    header("location: index.html");
+if (function_exists('mail')) {
+   
+    // Call the mail function
+    mail($to, $subject,$header, $message, );
+    
+} else {
+    // Handle the error gracefully
+   
+    echo "The mail function is not available on this server.";
 }
 ?>
